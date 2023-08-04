a4be969546ce3fa83b0d04745e8fe69db7bd5b3e
Preeti Paryani <preetiparyani29@gmail.com>
2023-08-04 20:54:00 +0530

    added a few more lines

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,4 +1,7 @@
-import { MouseEvent } from "react";
+// import { MouseEvent } from "react";
+//when we learned handle click function we needed the handleClick function and the this mouseevent too but know we don't need it so commenting out it.
+
+import { useState } from "react";
 
 function ListGroup() {
   let listItems = [
@@ -8,10 +11,17 @@ function ListGroup() {
     "chand sifarish",
     "Naino ne bandhi keasi door",
   ];
+  //we want to keep updation the value of selectedIndex which its possible just by declaring a function so commenting it out.
+  // let selectedIndex = 0;
+
+  //to keep the selectedIndex updated we have to use a hook named useState
+  //A hook enables us to tap into the built-in function in react
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   // listItems = [];
 
   //event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  // const handleClick = (event: MouseEvent) => console.log(event);
 
   const getMessage = () => {
     return listItems.length === 0 && <p>No items found</p>;
@@ -38,7 +48,18 @@ function ListGroup() {
         {/* every item in jsx needs a key to make the browser know what part of the code is being updated
          */}
         {listItems.map((item, index) => (
-          <li key={item} className="list-group-item" onClick={handleClick}>
+          <li
+            key={item}
+            // onClick={handleClick}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+          >
             {item}
           </li>
         ))}
