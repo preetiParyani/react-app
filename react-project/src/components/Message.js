dfc0d2de176ed4df9f298dd1ef8bc31b7a189fb3
Preeti Paryani <preetiparyani29@gmail.com>
2024-04-11 11:54:18 +0530

    Added a few new files in the react app

---
+++ react-project/src/components/Message.js
@@ -0,0 +1,12 @@
+import React from "react";
+// function message() {
+//   return (
+//     <h1>"Hello World!, I am Preeti a software developer learning react."</h1>
+//   );
+// }
+
+const Message = () => (
+  <h1>"Hello World!, I am Preeti a software developer learning react."</h1>
+);
+
+export default Message;
