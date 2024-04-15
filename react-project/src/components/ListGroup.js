342a4fb1acd97146bc2aca320d831e71ad90c8cb
Preeti Paryani <preetiparyani29@gmail.com>
2024-04-15 21:02:39 +0530

    done with today

--- react-project/src/components/ListGroup.js
+++ react-project/src/components/ListGroup.js
@@ -3,12 +3,18 @@ import React from "react";
 function ListGroup() {
   const items = ["India", "China", "Srilanka", "USA"];
 
+  const handleClick = (event) => console.log(event);
   return (
     <>
       <h1>List Group</h1>
-      <ul class="list-group">
-        {items.map((item) => (
-          <li>{item}</li>
+      {/* {items.length === 0 ? <p>No Items Found</p> : null} */}
+      {items.length === 0 && <p>No Items Found</p>}
+      {/* a concise way to write the same */}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
